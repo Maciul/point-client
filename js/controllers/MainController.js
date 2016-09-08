2a89cef6736a9479db4480e2eeea2a6ae568d55c
Pawel <Pawel.Maciulewski@gmail.com>
2016-09-07 22:56:39 -0600

    design and link to demo section

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,8 +1,14 @@
 angular
   .module("pointApp")
-  .controller('MainController', ['$scope', 'MainService', function($scope, MainService) {
+  .controller('MainController', ['$scope', '$location', '$anchorScroll', 'MainService',
+  function($scope, $location, $anchorScroll, MainService) {
     $scope.greeting = 'Hola!';
     $scope.form = {};
     $scope.toggleLogin = MainService.toggleLogin;
     $scope.toggleSignup = MainService.toggleSignup;
+
+    $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+    };
   }]);
