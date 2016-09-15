fcb5bcdf31e02e818906c07fe87cb590479894d8
Pawel <Pawel.Maciulewski@gmail.com>
2016-09-14 20:45:39 -0600

    add science base graph

--- js/controllers/DashboardController.js
+++ js/controllers/DashboardController.js
@@ -3,8 +3,21 @@ angular
   .controller('DashboardController', ['$scope','$location', 'DashboardService',
   function($scope, $location, DashboardService) {
     $scope.greeting = 'Hola!';
-    $scope.getScienceData = DashboardService.getScienceData;
+    $scope.scienceData = []
 
+
+    $scope.getScienceData = function() {
+      DashboardService.getScienceData().then(function(data) {
+        console.log(data)
+        $scope.scienceData = data;
+
+      });
+    };
+    $scope.data = [
+      {
+        values: [{"x":2008,"y":0},{"x":2009,"y":4.2},{"x":2010,"y":-0.3},{"x":2011,"y":-7.9},{"x":2012,"y":-9.2},{"x":2013,"y":-11.3},{"x":2014,"y":-13.1},{"x":2015,"y":-15.7},{"x":2016,"y":-18.5},{"x":2017,"y":-21.3},{"x":2018,"y":-24},{"x":2019,"y":-26.6},{"x":2020,"y":-29.2},{"x":2021,"y":-32.6},{"x":2022,"y":-35.8},{"x":2023,"y":-38.9},{"x":2024,"y":-41.9},{"x":2025,"y":-44.7},{"x":2026,"y":-47.9},{"x":2027,"y":-51},{"x":2028,"y":-54},{"x":2029,"y":-56.8},{"x":2030,"y":-59.4},{"x":2031,"y":-61.8},{"x":2032,"y":-64},{"x":2033,"y":-66.1},{"x":2034,"y":-68.2},{"x":2035,"y":-70.1},{"x":2036,"y":-71.9},{"x":2037,"y":-73.5},{"x":2038,"y":-75.1},{"x":2039,"y":-76.6},{"x":2040,"y":-78.1},{"x":2041,"y":-79.3},{"x":2042,"y":-80.4},{"x":2043,"y":-81.5},{"x":2044,"y":-82.6},{"x":2045,"y":-83.6},{"x":2046,"y":-84.4},{"x":2047,"y":-85.1},{"x":2048,"y":-85.8},{"x":2049,"y":-86.5},{"x":2050,"y":-87.2}],
+        key: 'sciencebase'
+      }]
     $scope.options3 = {
       chart: {
           type: 'lineChart',
@@ -18,6 +31,7 @@ angular
           x: function(d){ return d.x; },
           y: function(d){ return d.y; },
           useInteractiveGuideline: true,
+          forceY:([-10, 20]),
           dispatch: {
               stateChange: function(e){ console.log("stateChange"); },
               changeState: function(e){ console.log("changeState"); },
@@ -30,9 +44,10 @@ angular
           },
           yAxis: {
               axisLabel: 'Cumulative Reduction',
-
-
-              axisLabelDistance: 10
+              tickFormat: function(d) {
+                return d3.format(',.2f')(d)
+              },
+              axisLabelDistance: 10,
           },
           callback: function(chart){
               console.log("!!! lineChart callback !!!");
@@ -40,7 +55,7 @@ angular
       },
       title: {
           enable: true,
-          text: 'Title for Line Chart',
+          text: 'Science Base vs Portfolio',
           css: {
             color: 'darkblue'
           }
@@ -49,7 +64,7 @@ angular
 
       caption: {
           enable: true,
-          html: '<b>Figure 1.</b>',
+          html: '<b>Figure 1. Some awesome text here</b>',
           css: {
               'color': 'darkblue',
               'text-align': 'justify',
@@ -58,33 +73,4 @@ angular
       }
   };
 
-  $scope.data3 = sinAndCos();
-
-  /*Random Data Generator */
-  function sinAndCos() {
-      var sin = [],sin2 = [],
-          cos = [], test = [];
-
-      //Data is represented as an array of {x,y} pairs.
-      for (var i = 0; i < 100; i++) {
-          sin.push({x: i, y: Math.sin(i/10)});
-          sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
-          cos.push({x: i, y: .5 * Math.cos(i/10+ 2) + Math.random() / 10});
-      }
-
-      //Line chart data should be sent as an array of series objects.
-      return [
-
-        {
-            values: [{x: 1989, y: 0.0},{x: 1990, y: -4.57},{x: 1991, y: -10.57},{x: 1992, y: -14.57},{x: 1993, y: -18.57}],
-            key: 'Science Base', //key  - the name of the series.
-            color: '#ff7f0e',  //color - optional: choose your own line color.
-        },
-        {
-            values: [{x: 1989, y: 0.0001},{x: 1990, y: -1},{x: 1991, y: -17.57},{x: 1992, y: -21.57},{x: 1993, y: -30.57}],
-            key: 'Google', //key  - the name of the series.
-            color: '#aa7f0e'  //color - optional: choose your own line color.
-        }
-      ];
-  }
 }]);
--- js/services/dashboardService.js
+++ js/services/dashboardService.js
@@ -1,6 +1,6 @@
 angular
   .module("pointApp")
-  .service('DashboardService', ['$http', function($http) {
+  .factory('DashboardService', ['$http', function($http) {
 
     return  {
       getScienceData: getScienceData
@@ -8,11 +8,24 @@ angular
 
     function getScienceData() {
       console.log('something is happening')
-      $http({
+      return $http({
         method: 'GET',
-        url: 'https://point380.herokuapp.com/sciencebase'
+        url: 'https://point380.herokuapp.com/sciencebase/2008'
         }).then(function (data) {
-          console.log(data.data.data[0].target);
+          var cleanData = data.data.data[0];
+          var resultData = [];
+          var result = [
+            {
+              values: [],
+              key: ''
+            }
+          ];
+          result[0].key = 'Science Base';
+          var year = parseFloat(cleanData.year);
+          for (var i = 0; i < cleanData.target.length; i++) {
+            result[0].values.push({x: year + i, y: parseFloat(cleanData.target[i])});
+          }
+          return result;
           // this callback will be called asynchronously
           // when the response is available
         }, function errorCallback(error) {
