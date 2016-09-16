8dc9ddae1aae8d670f2e3a10db165d4a1fc3994e
Pawel <Pawel.Maciulewski@gmail.com>
2016-09-16 14:10:09 -0600

    change graph

--- js/services/dashboardService.js
+++ js/services/dashboardService.js
@@ -13,19 +13,21 @@ angular
         url: 'https://point380.herokuapp.com/sciencebase/2008'
         }).then(function (data) {
           var cleanData = data.data.data[0];
-          var resultData = [];
-          var result = [
-            {
-              values: [],
-              key: ''
-            }
-          ];
-          result[0].key = 'Science Base';
+          var values = [];
+          var values1 = [];
+          var result = []
           var year = parseFloat(cleanData.year);
           for (var i = 0; i < cleanData.target.length; i++) {
-            result[0].values.push({x: year + i, y: parseFloat(cleanData.target[i])});
+            values.push({x: year + i, y: parseFloat(cleanData.target[i])});
           }
-          return result;
+          for (i = 0; i < cleanData.target.length; i++) {
+            values1.push({x: year + i, y: parseFloat(cleanData.target[i]) + 10});
+          }
+          result.push({values:values, key:'sciencebase1'});
+          // result.push({values:values1, key:'sciencebase2'})
+          console.log(values)
+          console.log(result)
+          return result
           // this callback will be called asynchronously
           // when the response is available
         }, function errorCallback(error) {
